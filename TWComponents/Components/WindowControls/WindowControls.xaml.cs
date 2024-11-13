3942d678ab30e36c764b50957107b8bcb28e4b5e
zonex <zonex@>
2024-11-13 21:12:04 +0200

    changes

---
+++ TWComponents/Components/WindowControls/WindowControls.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace DrawerComponent.Components.WindowControls
+{
+    public partial class WindowControls : UserControl
+    {
+        public event Action<object, RoutedEventArgs> OnMinimizeClick = delegate { };
+        public event Action<object, RoutedEventArgs> OnCloseClick = delegate { };
+        public event Action<object, RoutedEventArgs> OnMaximizeClick = delegate { };
+
+        public WindowControls()
+        {
+            InitializeComponent();
+        }
+
+        private void Minimize_OnClick(object sender, RoutedEventArgs e)
+        {
+            OnMinimizeClick.Invoke(sender, e);
+        }
+
+        private void Close_OnClick(object sender, RoutedEventArgs e)
+        {
+            OnCloseClick.Invoke(sender, e);
+        }
+
+        private void Close_Maximize(object sender, RoutedEventArgs e)
+        {
+            OnMaximizeClick.Invoke(sender, e);
+        }
+    }
+}
